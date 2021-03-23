6ecf5670d2f6aefe55eaedb66abba1b3bb8e115f
Aleksander Dietrichson <dietrichson@gmail.com>
2021-03-23 14:46:08 -0300

    Added ref to courss table

---
+++ R/mdl_courses.R
@@ -0,0 +1,20 @@
+#' Get Courses
+#'
+#' Returns a reference to the (cached) course table, with the most relevant columns selected.
+#'
+#' For convenience a join with the category table is made, and "category_name" added
+#'
+#' @param con a database connection object
+#' @importFrom dplyr tbl
+#' @importFrom dplyr select
+#' @return A dbplyr reference object.
+#' @export
+mdl_courses <- function(
+  con = mdl_get_connection()
+) {
+  ret <- tbl(con, "courses") %>%
+    mutate(id = courseid) %>%
+    rename( courseid = courseId)
+  class(ret) <- c(class(ret), "mdl_courses")
+  ret
+}
