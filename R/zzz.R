25bd8835977ffc634cefafaf15b1cb658fab9ed7
Aleksander Dietrichson <dietrichson@gmail.com>
2026-02-26 16:11:24 +0000

    touch

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,7 @@
 }
 
 ## Declaring globals
+
 utils::globalVariables(c("ïd", "category", "category_name", "courseid", "created", "gender", "hist", "maxtime",
                          "mintime",
                          "name", "normalized_grade", "rawgrade", "rawgrademax", "thread_name", "userid", "word"))
