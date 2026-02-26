87cd138db5c41ff50d3184bac3faf90279b97fcb
Aleksander Dietrichson <dietrichson@gmail.com>
2026-02-26 15:27:59 +0000

    Added for CRAN compliance

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@
 }
 
 ## Declaring globals
-utils::globalVariables(c("category", "category_name", "courseid", "created", "gender", "hist", "maxtime",
+utils::globalVariables(c("ïd", "category", "category_name", "courseid", "created", "gender", "hist", "maxtime",
                          "mintime",
                          "name", "normalized_grade", "rawgrade", "rawgrademax", "thread_name", "userid", "word"))
