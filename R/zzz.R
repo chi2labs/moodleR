280c2bdcc46542f13a1a0a97265f6f841247d876
Aleksander Dietrichson <dietrichson@gmail.com>
2026-02-26 16:38:35 +0000

    removed non-ascii characters

--- R/zzz.R
+++ R/zzz.R
@@ -7,6 +7,6 @@
 
 ## Declaring globals
 
-utils::globalVariables(c("ïd", "category", "category_name", "courseid", "created", "gender", "hist", "maxtime",
+utils::globalVariables(c("id", "category", "category_name", "courseid", "created", "gender", "hist", "maxtime",
                          "mintime",
                          "name", "normalized_grade", "rawgrade", "rawgrademax", "thread_name", "userid", "word"))
